0345e88929ba0cda60b977729db9cfc27d1d3f4f
vrcalpha <190100403+vrcalphabet@users.noreply.github.com>
2026-05-17 19:52:54 +0900

    fix: ロゴがないログイン画面でエラーが発生しないように修正

--- src/cli/contents/scripts/global.ts
+++ src/cli/contents/scripts/global.ts
@@ -1,11 +1,12 @@
 export function globalScript() {
-  const logo = document.querySelector<HTMLAnchorElement>('#global-header > a')!
-  logo.href = '/challenges'
+  const logo = document.querySelector<HTMLAnchorElement>('#global-header > a')
+  if (!logo) return
 
   const results = document.createElement('a')
   results.classList.add('PT_results_link')
   results.href = '/skill_checks/results'
   results.textContent = '採点結果一覧'
 
+  logo.href = '/challenges'
   logo.after(results)
 }
